76b28c899bafbb0fdda42b0b6357b0e7191dab0c
Hilda Amponsah <hilda.amp@hotmail.com>
2020-07-27 16:35:15 +0100

    Interacting with PostgreSQL from Ruby

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,8 +1,9 @@
 class Bookmark
     def self.all
       [
-        "https://www.diagram.codes/",
+        "https://www.diagram.codes",
         "http://www.makersacademy.com",
+        "http://www.twitter.com",
         "http://www.google.com"
        ]
     end
--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -1,8 +1,9 @@
 feature "viewing bookmark" do
     scenario "to view bookmarks" do
         visit ('/bookmarks')
-        expect(page).to have_content("https://www.diagram.codes/")
+        expect(page).to have_content("https://www.diagram.codes")
         expect(page).to have_content("http://www.makersacademy.com")
+        expect(page).to have_content("http://www.twitter.com")
         expect(page).to have_content("http://www.google.com")
     end
 end
